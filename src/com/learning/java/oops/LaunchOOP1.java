630515c9485f0ef7c2926a91df8dab66532ca7a8
Vamshi <marthavamshikrishna1024@gmail.com>
2026-03-09 15:07:47 +0530

    Added OOP example with Student class and methods

--- src/com/learning/java/oops/LaunchOOP1.java
+++ src/com/learning/java/oops/LaunchOOP1.java
@@ -1,26 +1,35 @@
 package com.learning.java.oops;
 
-class Student {
+class Student
+{
+    //instance variable
+
     int id;
     String name;
     double age;
 
-    void sleep() {
-        id = 4;
-        int marks = 10;
+    void sleep()
+    {
+        id=4;
+        int marks=10;
         System.out.println("Student is sleeping");
     }
-
-    void study() {
-        age = 4.5;
+    void study()
+    {
+        //marks=15;
+        age=4.5;
         System.out.println("Student must study");
     }
 }
-
-public class LaunchOOP1 {
-    public static void main(String[] args) {
-        Student stu = new Student();
+public class LaunchOOP1
+{
+    public static void main(String[] args)
+    {
+        Student stu=new Student();
         stu.sleep();
         stu.study();
     }
+
 }
+
+
