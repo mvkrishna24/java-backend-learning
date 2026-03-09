365add289b9f9e77c30270723303f1d60dc94ea5
Vamshi <marthavamshikrishna1024@gmail.com>
2026-03-09 12:42:58 +0530

    Initial commit - Java backend learning setup

---
+++ src/com/learning/java/intro/Intro1.java
@@ -0,0 +1,12 @@
+package com.learning.java.intro;
+
+public class Intro1 {
+
+    public static void main(String[] args) {
+
+        // First Java Program
+        System.out.println("Hello Java Backend Learning!");
+
+    }
+
+}
