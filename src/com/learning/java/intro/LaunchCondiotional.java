15b5cc48fbff369bd8b2683bd61a29f0343bd276
Vamshi <marthavamshikrishna1024@gmail.com>
2026-03-09 14:39:56 +0530

    Day 01: Added Java primitive data types examples

---
+++ src/com/learning/java/intro/LaunchCondiotional.java
@@ -0,0 +1,64 @@
+package com.learning.java.intro;
+
+
+public class LaunchCondiotional
+{
+    public static void main(String[] args)
+    {
+//		System.out.println("Hello world");
+//
+//		int age = 14;
+//
+//		System.out.println("age");
+//		System.out.println(age);
+
+        byte a =45; // 1 byte
+
+        short b= 45;  // 2 byte
+        int c= 5;    // 4 byte
+
+        long d =45l;   // 8 byte
+
+        int age = 16 ; // the default data type of integer number in java in int
+
+        //Real Number
+
+
+        //45.5
+        float data=45.65656f;  // 4 bytes
+        double data2=5454.444444;  // 8 bytes
+
+        double avg = 45454.4; // the default data type of real number in java in double
+
+        char gf='f';
+        char bf = '4';
+
+        //chat bf = 'ha'; //illegal
+
+        //name --> class, method, variable anything except reserved word
+
+//		boolean agree= true;
+//
+//		boolean ag=false;
+
+
+
+        int num=15;
+        double average=454.54;
+        boolean dec=false;
+        char cr='1';
+
+
+        //Naming Convention
+
+//		class Name ===> TeluskoAlien  --> teluskoalien
+//		method name ==> camelcase --> main  --> mainMethod()  --> listTheAvg();
+//		variables ==> averageOfMarks --> avg --> marks --> salary  --> salaryOfEmployee
+//				  ==> average_of_marks  --> salary_of_employee
+
+
+
+
+    }
+
+}
